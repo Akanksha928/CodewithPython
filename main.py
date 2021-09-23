b9ced20e1341aa8bdc43ea1324c5003a9b0d186a
Akanksha Sharan <77259199+Akanksha928@users.noreply.github.com>
2021-09-23 10:27:47 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,46 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+answer = input("You're at a two way path. What so you do? Do you go left or right?\n")
+if(answer.lower()=='left'):
+  answer = input("You've reached a river. What do you do? Swim or wait?\n") 
+  if(answer.lower()=='wait'):
+    answer = input("Three doors appear in front of you? Which one do you enter? Which door? Yellow, blue or green?\n")
+    if(answer.lower()=='yellow'):
+      print("You win! Congratulations! <3")
+    elif(answer.lower()=='blue'):
+      print("It was a room full of tigers and you're dead. Game Over!")
+    else:
+      print("You fell into a blackhole. Game Over!")
+      
+  elif(answer.lower()=='swim'):
+    print("You drowned unfortunately since you never learnt how to swim. Game Over!")
+    
+elif(answer.lower()=='right'):
+  print("You fell into ditch and got eaten by a bear. Game Over!")
+
+
+
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
