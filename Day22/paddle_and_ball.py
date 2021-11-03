9379d02b69d5a9b642c7373ed042ecdf80dc7121
Akanksha Sharan <77259199+Akanksha928@users.noreply.github.com>
2021-11-03 22:20:06 +0530

    Add files via upload

---
+++ Day22/paddle_and_ball.py
@@ -0,0 +1,50 @@
+from turtle import Turtle
+
+
+class Paddle(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("square")
+        self.color("white")
+        self.shapesize(stretch_wid=5, stretch_len=1)
+
+    def position(self, x, y):
+        self.penup()
+        self.setposition(x, y)
+
+    def up(self):
+        new_y = self.ycor() + 20
+        self.goto(self.xcor(), new_y)
+
+    def down(self):
+        new_y = self.ycor() - 20
+        self.goto(self.xcor(), new_y)
+
+# Ball Class
+
+
+class Ball(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.color("white")
+        self.penup()
+        self.x_move = 0.15
+        self.y_move = 0.15
+
+    def move(self):
+        new_x = self.xcor() + self.x_move
+        new_y = self.ycor() + self.y_move
+        self.goto(new_x, new_y)
+
+    def collision_y(self):
+        self.y_move *= -1
+
+    def collision_x(self):
+        self.x_move *= -1
+
+    def reset_pos(self):
+        self.goto(0, 0)
+
+
+
