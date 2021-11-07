32da7463a87de6dab7f6bf523820616b63d73d7a
Akanksha Sharan <77259199+Akanksha928@users.noreply.github.com>
2021-11-07 23:14:00 +0530

    Add files via upload

---
+++ Day35/config.py
@@ -0,0 +1,4 @@
+OWM_api_key = "69f04e4613056b159c2761a9d9e664d2"
+twilio_account_sid = "AC5a9fb31fafbf30a105a965e4282b6e43"
+twilio_auth_token = "b7417dac8bd2daf83a2ac6946d9b25ab"
+my_phone_num = "+918056276150"
